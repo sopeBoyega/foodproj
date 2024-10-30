75fb29d74928d58c71ec6f63cba217fa9c657a16
S._.ope/ <sopeadegboyega@gmail.com>
2024-10-30 15:10:15 +0100

    Minor Fixes and Improvements

--- superstores/src/pages/Home/Home.jsx
+++ superstores/src/pages/Home/Home.jsx
@@ -1,19 +1,20 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Home.css'
 import Header from '../../components/Header/Header'
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'
+import { StoreContext } from '../../context/StoreContext'
+
 
 const Home = () => {
-  
+  const {food_list} = useContext(StoreContext)
   const [category,setCategory] = useState("All")
-  
   return (
     <div>
         <Header/>
         <ExploreMenu category={category}  setCategory={setCategory}/>
-       <FoodDisplay category = {category}/>
+       {food_list ? <FoodDisplay category = {category}/>  : <div className="spinner"></div>  } 
        <AppDownload/>
     </div>
   )

182c6f58662256e267305a283c37fbe2ff8290b6
S._.ope/ <sopeadegboyega@gmail.com>
2024-10-30 09:57:06 +0100

    Changed from BroswerRouter to HashRouter

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // Placing User order for frontend
  export const placeOrder = async (req,res) => {
 
-    const frontend_url  = "https://supestores.onrender.com/"
+    const frontend_url  = "https://supestores.onrender.com"
 try {
     const newOrder = new orderModel({
         userId:req.body.userId,
@@ -43,8 +43,8 @@ try {
     const session =await stripe.checkout.sessions.create({
         line_items:line_item,
         mode:'payment',
-        success_url:`${frontend_url}/verify?success=true&orderId=${newOrder._id}`,
-        cancel_url:`${frontend_url}/verify?success=false&orderId=${newOrder._id}`,
+        success_url:`${frontend_url}/#/verify?success=true&orderId=${newOrder._id}`,
+        cancel_url:`${frontend_url}/#/verify?success=false&orderId=${newOrder._id}`,
     })
     res.json({success:true,session_url:session.url})
 } catch (error) {
--- superstores/src/index.js
+++ superstores/src/index.js
@@ -2,17 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from "./context/StoreContext.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
-    {/* Context is a way to share or p
+ <>
+   {/* Context is a way to share or p
     ass data across components without having to manually pass props down at every level  */}
     {/* Here I use StoreContextProvider meaning the children components would have access to the contextValue that was passed */}
     <StoreContextProvider>
       <App />
     </StoreContextProvider>
-  </BrowserRouter>
+    </>
+  
+
 );
